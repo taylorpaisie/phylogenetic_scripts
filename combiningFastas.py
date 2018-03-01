a64c2411864c75521661640dcefcd66bd4b67a54
Taylor Paisie <tpaisie@ufl.edu>
2018-03-01 11:05:37 -0500

    Add files via upload
    Adds fasta files in a directory by alphabetical order.

--- combiningFastas.py
+++ combiningFastas.py
@@ -1,9 +1,9 @@
 from Bio import SeqIO
 import sys, glob
 
-file_list = glob.glob(sys.argv[1])
+file_list = sorted(glob.glob("*"))
 
-with open(sys.argv[2], 'w') as w_file:
+with open(sys.argv[1], 'w') as w_file:
     for filen in file_list:
         with open(filen, 'rU') as o_file:
             seq_records = SeqIO.parse(o_file, 'fasta')
