5f31adcc2f7f28c9da8862814f604bee24a04745
Taylor Paisie <tpaisie@ufl.edu>
2018-04-25 08:48:40 -0400

    Add files via upload
    Updated, general fixes.

--- extractFromAccessionCountryDate.py
+++ extractFromAccessionCountryDate.py
@@ -3,11 +3,12 @@
 # python /Volumes/IT/PhylodynamicLab/Programs/scripts_python/extractFromAccessionCountryDate.py INPUTFILE OUTPUTFILE
 
 
-import sys
+from ftplib import FTP
+import sys, os
 from Bio import SeqIO, Entrez
 
 print("NCBI needs your email.")
-Entrez.email = raw_input(str(">"))
+Entrez.email = input(str(">"))
 print("Now the governement has your email address. Muahaha.")
 
 
@@ -19,21 +20,21 @@ for line in id_list:
     line = line.rstrip("\n")
     accessions.append(line)
 
-gb_output = open(sys.argv[2] + ".gb", "w")
+gb_output = open(sys.argv[2] + ".xml", "w")
 for gb_num in accessions:
-    handle = Entrez.efetch(db="nucleotide", id=gb_num, rettype="gb", retmode="text")
-    gb_seqs = SeqIO.parse(handle, "gb")
-    SeqIO.write(gb_seqs, gb_output, "gb")
+    handle = Entrez.efetch(db="nucleotide", id=gb_num, rettype="xml", retmode="xml")
+    gb_seqs = SeqIO.parse(handle, "seqxml")
+    SeqIO.write(gb_seqs, gb_output, "seqxml")
 handle.close()
 gb_output.close()
 #genbank file is now downloaded
 
 #extract country and collection date from genbank file
-gb_file = open(sys.argv[2] + ".gb", "r")
+gb_file = open(sys.argv[2] + ".xml", "r")
 #extract country and collection date from genbank file
 #take out accession numbers and country in a text file
 save_country = open(sys.argv[2] + "_country.txt", "w")
-for gb_record in SeqIO.parse(gb_file, "genbank") :
+for gb_record in SeqIO.parse(gb_file, "seqxml") :
     # now do something with the record
         save_country.write(gb_record.name+"\t"),   # print genbank accession number with no new line
         for feat in gb_record.features:
@@ -46,11 +47,11 @@ for gb_record in SeqIO.parse(gb_file, "genbank") :
 save_country.close()
 gb_file.close()
 
-gb_file = open(sys.argv[2] + ".gb", "r")
+gb_file = open(sys.argv[2] + ".xml", "r")
 #take out accession numbers and collection date in a text file
 # parse genbank file
 save_date = open(sys.argv[2] + "_date.txt", "w")
-for gb_record in SeqIO.parse(gb_file, "genbank"):
+for gb_record in SeqIO.parse(gb_file, "seqxml"):
     # now do something with the record
         save_date.write(gb_record.name+"\t"),   # print genbank accession number with no new line
         for feat in gb_record.features:
