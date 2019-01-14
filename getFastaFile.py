c6a3fc6e9a7f530d8e7152435ef5819bcd316bd0
Taylor Paisie <tpaisie@ufl.edu>
2019-01-14 15:52:31 -0500

    Add files via upload
    Updated for the new updates from NCBI

--- getFastaFile.py
+++ getFastaFile.py
@@ -1,14 +1,21 @@
 #first uploading the list of IDs using EPost
 #can refer to the long list of IDs and download the associated data with EFetch
-from Bio import Entrez, SeqIO
-import sys
+import sys, os
+import urllib
+import time
+from Bio import SeqIO, Entrez
+from Bio.Seq import Seq
+from Bio.SeqFeature import SeqFeature
+from ftplib import FTP
+from urllib.error import HTTPError
 
 
+Entrez.api_key = "b8558abb22f17ea43a6fc07d48097c50a208"
 print("NCBI needs your email.")
 Entrez.email = input(str(">"))
 print("Now the governement has your email address. Muahaha.")
 
-id_list = open(sys.argv[1])
+id_list = open(sys.argv[1], 'r')
 
 accessions = []
 for line in id_list:
@@ -17,7 +24,7 @@ for line in id_list:
 
 fasta_output = open(sys.argv[2], "w")
 for num in accessions:
-    handle = Entrez.efetch(db="nucleotide", id=num, rettype="fasta")
+    handle = Entrez.efetch(db="nuccore", id=num, rettype="fasta")
     fasta_seqs = SeqIO.parse(handle, "fasta")
     SeqIO.write(fasta_seqs, fasta_output, "fasta")
 handle.close()
@@ -26,6 +33,3 @@ fasta_output.close()
 
 
 #fasta file
-
-
-
